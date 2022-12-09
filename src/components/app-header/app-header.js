16295cf30e20d38406c8a5bffb1ede1ea89be503
Vadim Savinov <vasaykh@yandex.ru>
2022-12-10 02:03:21 +0500

    feat: добавить верстку AppHeader

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -3,19 +3,41 @@ import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
 import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import { Box } from '@ya.praktikum/react-developer-burger-ui-components';
+
 
 import appHeaderStyles from './app-header-styles.module.css';
 
 class AppHeader extends React.Component {
   render() {
     return (
-      <header className={appHeaderStyles.header}>
-        <BurgerIcon type="primary" />
-        <p className="text text_type_main-default">Конструктор</p>
+      <header className={appHeaderStyles.header + ' ' + appHeaderStyles.blocks}>
+        <div className={'mr-2 ' + appHeaderStyles.blocks}>
+          <div className="mt-4 mb-4 ml-5">
+            <BurgerIcon type="primary" />
+          </div>
+          <p className="text text_type_main-default mt-4 mr-5 mb-4 ml-2">
+            Конструктор
+          </p>
+        </div>
+        <div className={appHeaderStyles.blocks}>
+          <div className="mt-4 mb-4 ml-5">
+            <ListIcon type="primary" />
+          </div>
+          <p className="text text_type_main-default mt-4 mr-5 mb-4 ml-2">
+            Лента заказов
+          </p>
+        </div>
+        <div className={appHeaderStyles.logo}>
         <Logo />
-        <ListIcon type="primary" />
-        <ProfileIcon type="primary" />
+        </div>
+        <div className={appHeaderStyles.blocks}>
+          <div className="mt-4 mb-4 ml-5">
+            <ProfileIcon type="primary" />
+          </div>
+          <p className="text text_type_main-default mt-4 mr-5 mb-4 ml-2">
+            Личный кабинет
+          </p>
+        </div>
       </header>
     );
   }
