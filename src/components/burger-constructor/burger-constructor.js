ad18b7a54c58084c67e0e521530977e469441fa1
Vadim Savinov <vasaykh@yandex.ru>
2022-12-15 23:26:08 +0500

    fix: сконструировать бургер

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -9,48 +9,60 @@ import burgerConstructorStyles from './burger-constructor-styles.module.css';
 function BurgerConstructor(props) {
   return (
     <section className={burgerConstructorStyles.section}>
+      <div
+        className={
+          burgerConstructorStyles.blockBun +
+          ' pr-4 ' +
+          burgerConstructorStyles.blockItem
+        }
+      >
+        <ConstructorElement
+          text={props.data[0].name + ' (верх)'}
+          thumbnail={props.data[0].image}
+          price={props.data[0].price}
+          type='top'
+          isLocked='undefined'
+        />
+      </div>
       <ul className={burgerConstructorStyles.blockTipes}>
-        {props.data.map((item, index, array) => {
-          let typeItem;
-          let isLockedItem;
-          let visibilDrag;
-          if (index === 0) {
-            typeItem = 'top';
-            isLockedItem = 'undefined';
-            visibilDrag = burgerConstructorStyles.blockDrag;
-          } else {
-            if (index === array.length - 1) {
-              typeItem = 'bottom';
-              isLockedItem = 'undefined';
-              visibilDrag = burgerConstructorStyles.blockDrag;
-            } else {
-              typeItem = 'undefined';
-              isLockedItem = '';
-              visibilDrag = '';
-            }
-          }
-          return (
-            <li
-              className={burgerConstructorStyles.blockString + ' pr-2'}
-              key={item._id}
-            >
-              <div className={visibilDrag}>
+        {props.data.map((item) => {
+          if (item.type === 'sauce' || item.type === 'main') {
+            return (
+              <li
+                className={burgerConstructorStyles.blockString + ' pr-2'}
+                key={item._id}
+              >
                 <DragIcon type='primary' />
-              </div>
-              <div className={burgerConstructorStyles.blockItem}>
-                <ConstructorElement
-                  text={item.name}
-                  thumbnail={item.image}
-                  price={item.price}
-                  type={typeItem}
-                  isLocked={isLockedItem}
-                />
-              </div>
-            </li>
-          );
+                <div className={burgerConstructorStyles.blockItem}>
+                  <ConstructorElement
+                    text={item.name}
+                    thumbnail={item.image}
+                    price={item.price}
+                    type='undefined'
+                    isLocked=''
+                  />
+                </div>
+              </li>
+            );
+          }
         })}
       </ul>
-      <div className={burgerConstructorStyles.blockPrice + ' mt-10 mb-10 pr-4'}>
+      <div
+        className={
+          burgerConstructorStyles.blockBun +
+          ' pr-4 ' +
+          burgerConstructorStyles.blockItem
+        }
+      >
+        <ConstructorElement
+          text={props.data[0].name + ' (низ)'}
+          thumbnail={props.data[0].image}
+          price={props.data[0].price}
+          type='bottom'
+          isLocked='undefined'
+        />
+      </div>
+      <div className={burgerConstructorStyles.blockPrice + ' mt-6 mb-10 pr-4'}>
         <p className='text text_type_digits-medium pr-2'>610</p>
         <div className={burgerConstructorStyles.blockCurrencyIcon + ' mr-10'}>
           <CurrencyIcon type='primary' />
