ea6b4c72481acd5135c1d5bfcb601e31edf9b650
Vadim Savinov <vasaykh@yandex.ru>
2022-12-16 23:02:36 +0500

    feat: подключиться к API

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppHeader from '../app-header/app-header.js';
 import BurgerMain from '../burger-main/burger-main.js';
-import { data } from '../../utils/data';
+//import { data } from '../../utils/data';
 import appStyles from './app-styles.module.css';
 
+const urlDomen = 'https://norma.nomoreparties.space/api/ingredients';
+
 function App() {
+  const [state, setState] = useState({
+    isLoading: false,
+    hasError: false,
+    data: [],
+  });
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    try {
+      setState({ ...state, hasError: false, isLoading: true });
+      fetch(urlDomen)
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res.data);
+          const data = res.data;
+          setState({ ...state, data, isLoading: false });
+        })
+        .catch((e) => {
+          setState({ ...state, hasError: true, isLoading: false });
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <AppHeader />
-      <BurgerMain data={data} />
+      {state.isLoading && 'Загрузка...'}
+      {state.hasError && 'Произошла ошибка'}
+      {!state.isLoading && !state.hasError && state.data.length && (
+        <>
+          <AppHeader />
+          <BurgerMain data={state.data} />
+        </>
+      )}
     </>
   );
 }
