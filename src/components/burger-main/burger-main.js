a003fb971f74d3e704501e0367bf365eb63f9f59
Vadim Savinov <vasaykh@yandex.ru>
2023-02-05 10:38:37 +0500

    feat: оборнуть main в DndProvider

--- src/components/burger-main/burger-main.js
+++ src/components/burger-main/burger-main.js
@@ -1,4 +1,6 @@
 //import React, {  useMemo } from 'react';
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import BurgerIngredients from '../burger-ingredients/burger-ingredients.js';
 import BurgerConstructor from '../burger-constructor/burger-constructor.js';
 
@@ -6,9 +8,11 @@ import burgerMainStyles from './burger-main-styles.module.css';
 
 export default function BurgerMain() {
   return (
+    <DndProvider backend={HTML5Backend}>
     <main className={burgerMainStyles.blocks}>
       <BurgerIngredients />
       <BurgerConstructor />
     </main>
+    </DndProvider>
   );
 }
